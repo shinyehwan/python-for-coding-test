f69eaf3e8803434d88f6bbee1c42e2b699ce93d6
shinyehwan <jghisjl007@naver.com>
2022-07-04 16:27:58 +0900

    다시 시작

---
+++ thisiscodingtest/다이나믹 프로그래밍/개미전사.py
@@ -0,0 +1,15 @@
+# 8-6
+# 오랜만에 복습이라 이해하는데 시간이 오래걸렸다.
+
+n = int(input())
+array = list(map(int, input().split()))
+d = [0] * 100
+
+d[0] = array[0]
+d[1] = max(array[0], array[1])
+
+for i in range(2,n):
+    d[i] = max(d[i-1], d[i-2] + array[i])
+
+print(d[n-1])
+
