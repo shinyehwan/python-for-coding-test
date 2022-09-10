02b53fd9c34266c5c4cd2b873607d1fd7921e8b2
shinyehwan <jghisjl007@naver.com>
2022-09-11 01:51:36 +0900

    수정해야함

--- baekjoon/이분탐색/2512.py
+++ baekjoon/이분탐색/2512.py
@@ -1,3 +1,42 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-x = map(int, input().split())
+arrays = list(map(int, input().split()))
 m = int(input())
+
+def sum(upper_bound : int):
+    result = 0
+    for array in arrays:
+        if upper_bound <= array:
+            result += upper_bound
+        else:
+            result += array
+    
+    return result
+
+
+start = 0
+end = max(arrays)
+target = -1
+
+while start <= end:
+    mid = start + end // 2
+    if sum(mid) <= m:
+        target = mid
+        start = mid + 1
+    else:
+        end = mid - 1
+
+answer = -1
+for array in arrays:
+    given = min(array, target)
+    answer = max(answer, given)
+
+print(answer)
+
+
+
+
+
+
