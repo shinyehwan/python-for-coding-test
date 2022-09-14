a3552c7f2428da33fb7fa72cb5024ceb16def12a
shinyehwan <jghisjl007@naver.com>
2022-09-15 04:50:48 +0900

    수정

--- baekjoon/이분탐색/2512.py
+++ baekjoon/이분탐색/2512.py
@@ -1,32 +1,61 @@
+# n = int(input())
+# arrays = list(map(int, input().split()))
+# m = int(input())
+
+# def sum(upper_bound):
+#     result = 0
+#     for array in arrays:
+#         if upper_bound <= array:
+#             result += upper_bound
+#         else:
+#             result += array
+#     return result
+
+# start = 0
+# end = max(arrays)
+# target = -1
+
+# while start <= end:
+#     mid = start + end // 2
+#     if sum(mid) <= m:
+#         target = mid
+#         start = mid + 1
+#     else:
+#         end = mid - 1
+# print(end)
+
+
+# answer = -1
+# for array in arrays:
+#     given = min(array, target)
+#     answer = max(answer, given)
+# print(answer)
+
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 arrays = list(map(int, input().split()))
 m = int(input())
 
-def sum(upper_bound):
-    result = 0
-    for array in arrays:
-        if upper_bound <= array:
-            result += upper_bound
-        else:
-            result += array
-    
-    return result
-
 start = 0
 end = max(arrays)
-target = -1
-
 while start <= end:
     mid = start + end // 2
-    if sum(mid) <= m:
-        target = mid
+    result = 0
+
+    for array in arrays:
+        if array <= mid:
+            result += array
+        else:
+            result += mid
+
+    if result <= m:
         start = mid + 1
     else:
         end = mid - 1
+
 print(end)
-
-
-
 
 
 
