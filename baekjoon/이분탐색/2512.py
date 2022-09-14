535e8865d4ed0957a79baca54695cdec1cfcd42c
shinyehwan <jghisjl007@naver.com>
2022-09-15 06:46:52 +0900

    아주 간단한 괄호로 인한 오류 수정

--- baekjoon/이분탐색/2512.py
+++ baekjoon/이분탐색/2512.py
@@ -1,28 +1,31 @@
-# n = int(input())
-# arrays = list(map(int, input().split()))
-# m = int(input())
+import sys
+input = sys.stdin.readline
 
-# def sum(upper_bound):
-#     result = 0
-#     for array in arrays:
-#         if upper_bound <= array:
-#             result += upper_bound
-#         else:
-#             result += array
-#     return result
+n = int(input())
+arrays = list(map(int, input().split()))
+m = int(input())
 
-# start = 0
-# end = max(arrays)
-# target = -1
+def sum(upper_bound):
+    result = 0
+    for array in arrays:
+        if upper_bound <= array:
+            result += upper_bound
+        else:
+            result += array
+    return result
 
-# while start <= end:
-#     mid = start + end // 2
-#     if sum(mid) <= m:
-#         target = mid
-#         start = mid + 1
-#     else:
-#         end = mid - 1
-# print(end)
+start = 0
+end = max(arrays)
+target = -1
+
+while start <= end:
+    mid = (start + end) // 2
+    if sum(mid) <= m:
+        target = mid
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
 
 
 # answer = -1
@@ -31,31 +34,32 @@
 #     answer = max(answer, given)
 # print(answer)
 
-import sys
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
 
-n = int(input())
-arrays = list(map(int, input().split()))
-m = int(input())
+# n = int(input())
+# arrays = list(map(int, input().split()))
+# m = int(input())
 
-start = 0
-end = max(arrays)
-while start <= end:
-    mid = start + end // 2
-    result = 0
+# start = 0
+# end = max(arrays)
+# while start <= end:
+#     mid = (start + end) // 2
+#     result = 0
 
-    for array in arrays:
-        if array <= mid:
-            result += array
-        else:
-            result += mid
+#     for i in arrays:
+#         if i <= mid:
+#             result += i
+#         else:
+#             result += mid
 
-    if result <= m:
-        start = mid + 1
-    else:
-        end = mid - 1
+#     if result <= m:
+#         start = mid + 1
+#     else:
+#         end = mid - 1
 
-print(end)
+# print(end)
+
 
 
 
