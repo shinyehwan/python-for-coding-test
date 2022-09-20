69ee6d8b7dd84eb6bc8a4ffd9fcdd2811d743633
shinyehwan <jghisjl007@naver.com>
2022-09-20 18:50:55 +0900

    review

--- thisiscodingtest/그리디/(2)3-2.py
+++ thisiscodingtest/그리디/(2)3-2.py
@@ -1,4 +1,44 @@
+# import sys
+# input = sys.stdin.readline
+# n,m,k = list(map(int, input().split()))
+# data = list(map(int, input().split()))
+
+# data.sort()
+
+# first = data[n-1]
+# second = data[n-2]
+
+# result = 0
+
+# while True:
+#     for i in range(k):
+#         if m == 0:
+#             break
+#         result += first
+#         m -= 1
+#     if m == 0:
+#         break
+#     result += second
+#     m -= 1
+
+# print(result)
+
+
+import sys
+input = sys.stdin.readline
 n,m,k = list(map(int, input().split()))
 data = list(map(int, input().split()))
 
 data.sort()
+
+first = data[n-1]
+second = data[n-2]
+
+result = 0
+
+count = (m // (k+1)) * k + (m % (k+1))
+
+result += count * first
+result += (m-count) * second
+
+print(result)
